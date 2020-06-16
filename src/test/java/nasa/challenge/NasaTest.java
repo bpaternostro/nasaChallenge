f86c2ce864b202ac83dda646a147a3412a792105
bpaternostro <bruno.paternostro@gmail.com>
2020-06-16 17:03:00 -0300

    update nada test

--- src/test/java/nasa/challenge/NasaTest.java
+++ src/test/java/nasa/challenge/NasaTest.java
@@ -3,106 +3,114 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Test;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.Assert;
 import nasa.util.Constant;
 import retrofit2.Call;
 
-public class NasaTest 
-extends TestBase
-{
-		
+public class NasaTest extends TestBase {
 
-	//prueba desde Intellij
-    //1-Retrieve the first 10 Mars photos made by "Curiosity" on 1000 Martian sol.
+	String earthDateCheckMsg = "It was impossible to catch earth_date.";
+
+	// prueba desde Intellij
+	// 1-Retrieve the first 10 Mars photos made by "Curiosity" on 1000 Martian sol.
+	@Parameters({ "sol", "page" })
 	@Test
-    public void test1000MartianSol ()
-    {
-    	Call<ArrayPhotos> call = jsonFromApi.getPhotos(Constant.sol,Constant.page,Constant.token);
-    	List<Photo> photos = glo.getPhotos(call); 
-    	glo.listAllPhotos(photos);
-    	    	    	    	
-    };
-    
-    
-    //2-Retrieve the first 10 Mars photos made by "Curiosity" on Earth date equal to 1000 Martian sol.
-    @Test
-    public void test1000MartianSolWithEarthDate ()
-    {
-    	     	    	
-    	//getting the earh date from martian sol 1000
-    	Call<ArrayPhotos> callAux = jsonFromApi.getPhotos(Constant.sol,Constant.page,Constant.token);
-    	String earth_date = glo.getEarthDateMartianSol(callAux);
-		    	  	    	
-		if(earth_date!=null) {				    	
-			Call<ArrayPhotos> call = jsonFromApi.getPhotosByEarthDate(earth_date,Constant.page,Constant.token);
-			List<Photo> photos = glo.getPhotos(call); 
-	    	glo.listAllPhotos(photos);		
+	public void test1000MartianSol(int sol, int page) {
+		Call<ArrayPhotos> call = jsonFromApi.getPhotos(sol, page, Constant.TOKEN);
+		List<Photo> photos = glo.getPhotos(call);
+		glo.listAllPhotos(photos);
+
+	};
+
+	// 2-Retrieve the first 10 Mars photos made by "Curiosity" on Earth date equal
+	// to 1000 Martian sol.
+	@Parameters({ "sol", "page" })
+	@Test
+	public void test1000MartianSolWithEarthDate(int sol, int page) {
+
+		// getting the earh date from martian sol 1000
+		Call<ArrayPhotos> callAux = jsonFromApi.getPhotos(sol, page, Constant.TOKEN);
+		String earth_date = glo.getEarthDateMartianSol(callAux);
+
+		if (earth_date != null) {
+			Call<ArrayPhotos> call = jsonFromApi.getPhotosByEarthDate(earth_date, page, Constant.TOKEN);
+			List<Photo> photos = glo.getPhotos(call);
+			glo.listAllPhotos(photos);
+		} else {
+			log.info("Test - test1000MartianSolWithEarthDate: " + earthDateCheckMsg);
+			Assert.assertTrue(false, earthDateCheckMsg);
 		}
-		else
-		{
-			assertTrue("It was impossible to catch earth_date.",false);
+
+	}
+
+	// 3-Retrieve and compare the first 10 Mars photos made by "Curiosity" on 1000
+	// sol and on Earth date equal to 1000 Martian sol.
+	@Parameters({ "sol", "page" })
+	@Test
+	public void testComparisonList(int sol, int page) {
+
+		String earth_date = null;
+		List<Photo> photos = null;
+		List<Photo> photos2 = null;
+
+		Call<ArrayPhotos> call1 = jsonFromApi.getPhotos(sol, page, Constant.TOKEN);
+		photos = glo.getPhotos(call1);
+
+		// getting the earh date from martian sol 1000
+		Call<ArrayPhotos> callAux = jsonFromApi.getPhotos(sol, page, Constant.TOKEN);
+		earth_date = glo.getEarthDateMartianSol(callAux);
+
+		if (earth_date != null) {
+			Call<ArrayPhotos> call2 = jsonFromApi.getPhotosByEarthDate(earth_date, page, Constant.TOKEN);
+			photos2 = glo.getPhotos(call2);
+		} else {
+			log.info("Test - testComparisonList: " + earthDateCheckMsg);
+			Assert.assertTrue(false, earthDateCheckMsg);
 		}
-    	    	
-    }
-    
-    //3-Retrieve and compare the first 10 Mars photos made by "Curiosity" on 1000 sol and on Earth date equal to 1000 Martian sol.
-   	@Test
-    public void testComparisonList ()
-    {
-    	     	
-    	
-    	String earth_date = null;
-    	List<Photo> photos = null;
-    	List<Photo> photos2 = null;
-    	
-    	Call<ArrayPhotos> call1 = jsonFromApi.getPhotos(Constant.sol,Constant.page,Constant.token);
-    	photos = glo.getPhotos(call1);
-    	
-    	//getting the earh date from martian sol 1000
-    	Call<ArrayPhotos> callAux = jsonFromApi.getPhotos(Constant.sol,Constant.page,Constant.token);
-    	earth_date = glo.getEarthDateMartianSol(callAux);
-		    	  	    	
-		if(earth_date!=null) {				    	
-			Call<ArrayPhotos> call2 = jsonFromApi.getPhotosByEarthDate(earth_date,Constant.page,Constant.token);
-	    	photos2 = glo.getPhotos(call2); 	    		
-		}
-		else
-		{
-			assertTrue("It was impossible to catch earth_date.",false);
-		}
-		
-		//Assuming that both list should be equals 		
-		assertTrue("Checking List of photos",photos.equals(photos2));		
-  	    	
-    }    
-        
-    //4-Validate that the amounts of pictures that each "Curiosity" camera took on 1000 Mars sol is not greater than 10 times the amount taken by other cameras on the same date.
-    @Test
-	public void testValidateAmountOfPhotos ()
-    {
-    	     	
-    	String earth_date = null;
-    	String cameraToCompare = "curiosity";
-    	List<Photo> photos = null;    	
-    	Map<String, Long> amounts= new HashMap<String, Long>();
-    	
-    	//getting the earh date from martian sol 1000
-    	Call<ArrayPhotos> callAux = jsonFromApi.getPhotos(Constant.sol,Constant.page,Constant.token);
-    	earth_date = glo.getEarthDateMartianSol(callAux);
-		    	  	    	
-    	if(earth_date==null) {
-			assertTrue("It was impossible to catch earth_date.",false);
+
+		// Assuming that both list should be equals
+		Boolean result = photos.equals(photos2);
+		String msg = "Test - testComparisonList: Checking List of photos " + result;
+		log.info(msg);
+		Assert.assertTrue(result, msg);
+
+	}
+
+	// 4-Validate that the amounts of pictures that each "Curiosity" camera took on
+	// 1000 Mars sol is not greater than 10 times the amount taken by other cameras
+	// on the same date.
+	@Test
+	public void testValidateAmountOfPhotos(int sol, int page) {
+
+		String earth_date = null;
+		String cameraToCompare = "curiosity";
+		List<Photo> photos = null;
+		Map<String, Long> amounts = new HashMap<String, Long>();
+
+		// getting the earh date from martian sol 1000
+		Call<ArrayPhotos> callAux = jsonFromApi.getPhotos(sol, page, Constant.TOKEN);
+		earth_date = glo.getEarthDateMartianSol(callAux);
+
+		if (earth_date == null) {
+			log.info("Test - testValidateAmountOfPhotos: " + earthDateCheckMsg);
+			Assert.assertTrue(false, earthDateCheckMsg);
 			return;
 		}
-    	
-    	for(String camera:Constant.cameras) {    		    								    	
-			Call<ArrayPhotos> call2 = jsonFromApi.getPhotosByCamera(camera, Constant.sol, earth_date,Constant.token);
-		    photos = glo.getAllPhotos(call2); 	
-		    amounts.put(camera, glo.getTotalAmountPhotos(photos));
-    	}
-		  	    
-  	    assertTrue("Checking amount of photos: ",glo.checkAmounts(amounts,cameraToCompare));
-  	      	    
-    }
-    
+
+		for (String camera : Constant.CAMERAS) {
+			Call<ArrayPhotos> call2 = jsonFromApi.getPhotosByCamera(camera, sol, earth_date, Constant.TOKEN);
+			photos = glo.getAllPhotos(call2);
+			amounts.put(camera, glo.getTotalAmountPhotos(photos));
+		}
+
+		Boolean result = glo.checkAmounts(amounts, cameraToCompare);
+		String msg = "Test - testValidateAmountOfPhotos: Checking amount of photos " + result;
+		log.info(msg);
+		Assert.assertTrue(result, msg);
+
+	}
+
 }
+

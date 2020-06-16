6ac4890e675f7dcbdef865a6c6a77f6f5408d124
bpaternostro <bruno.paternostro@gmail.com>
2020-06-16 16:59:41 -0300

    update constant

--- src/main/java/nasa/util/Constant.java
+++ src/main/java/nasa/util/Constant.java
@@ -2,10 +2,9 @@ package nasa.util;
 
 public class Constant {
 	
-	public final static String TOKEN="uLqW3EQfnm9IqYLhzEHUckWUSnzeWvkd1M4GdNcE";
-	public final static int SOL=1000;
-	public final static int PAGE=1;
+	public final static String TOKEN="uLqW3EQfnm9IqYLhzEHUckWUSnzeWvkd1M4GdNcE";	
 	public final static String[] CAMERAS =  {"curiosity","opportunity","spirit"};
 	
 	
 }
+
